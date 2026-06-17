95dfcce306c6229c95b999f29a4d6770c7ad6627
Tomas Colombo <tomas.colombo@cisb.com.ar>
2026-06-17 12:25:15 -0300

    feat: Init

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import 'zone.js';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideAnimations()],
-}).catch((error) => console.error(error));
